a1eb1a4120d70bab8af00f5bcf62c1a01cf5c4ea
jasonfarfan <123587376+jasonfarfan@users.noreply.github.com>
2023-04-13 05:59:45 +0000

    finished project

---
+++ array.py
@@ -0,0 +1,54 @@
+# problem 1: print your first and last name
+print("First Name: John")
+print("Last Name: Doe")
+
+# problem 2: create an array named 'cars'
+cars = ['Ford', 'Chrysler', 'Dodge', 'Ram', 'Jeep', 'Chevy', 'GMC']
+
+# problem 3: print the array to the console
+print(cars)
+
+# problem 4: print the length of the array to the console
+print("Length of cars array:", len(cars))
+
+# problem 5: Append Buick to the Array
+cars.append('Buick')
+
+# problem 6: print the array to the console
+print(cars)
+
+# problem 7: Print the 4th element in the array
+print("4th element in cars array:", cars[3])
+
+# problem 8: Insert 'Toyota' into element 3 in the array
+cars.insert(2, 'Toyota')
+
+# problem 9: print the array to the console
+print(cars)
+
+# problem 10: Remove element 5 of the array
+cars.pop(4)
+
+# problem 11: print the array to the console
+print(cars)
+
+# problem 12: Sort the array in ascending order
+cars.sort()
+
+# problem 13: print the array to the console
+print(cars)
+
+# problem 14: Sort the array in descending order
+cars.sort(reverse=True)
+
+# problem 15: print the array to the console
+print(cars)
+
+# problem 16: create a variable called my_array_length with a value of the cars array length
+my_array_length = len(cars)
+
+# problem 17: create a variable called array_string with a value of 'The length of my array is '
+array_string = 'The length of my array is '
+
+# problem 18: print array_string concatenated with my_array_length to the console
+print(array_string + str(my_array_length))
